7bd21c0bb532933801a4e86f860d13d74ec52c97
Groza Danut <Danut@192.168.11.195>
2024-03-09 08:27:42 +0200

    Release 0.0.2 with ParameterStringBuilder

--- commons/src/main/java/ro/linic/util/commons/ParameterStringBuilder.java
+++ commons/src/main/java/ro/linic/util/commons/ParameterStringBuilder.java
@@ -1,24 +1,32 @@
 package ro.linic.util.commons;
 
+import static ro.linic.util.commons.PresentationUtils.EMPTY_STRING;
+
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public class ParameterStringBuilder
 {
-	public static String getParamsString(final Map<String, String> params) throws UnsupportedEncodingException
+	private static final Logger log = Logger.getLogger(ParameterStringBuilder.class.getName());
+	
+	public static String getParamsString(final Map<String, String> params)
 	{
-		final StringBuilder result = new StringBuilder();
-
-		for (final Map.Entry<String, String> entry : params.entrySet())
-		{
-			result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-			result.append("=");
-			result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-			result.append("&");
-		}
-
-		final String resultString = result.toString();
-		return resultString.length() > 0 ? resultString.substring(0, resultString.length() - 1) : resultString;
+		if (params == null || params.isEmpty())
+			return EMPTY_STRING;
+		
+		return params.entrySet().stream()
+				.map(entry -> {
+					try {
+						return URLEncoder.encode(entry.getKey(), "UTF-8") + "=" + URLEncoder.encode(entry.getValue(), "UTF-8");
+					} catch (final UnsupportedEncodingException e) {
+						log.log(Level.SEVERE, "Error url encoding: "+entry, e);
+						return EMPTY_STRING;
+					}
+				})
+				.collect(Collectors.joining("&", "?", EMPTY_STRING));
 	}
 }
